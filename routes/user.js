714f341353423d1fc544720d684141801c9c4a1f
MD AIJAJ <41102052+mdaijaj@users.noreply.github.com>
2021-03-01 17:31:06 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const users=require('../controller/user')
 const router=express()
 
 router.post('/add_detail', users.add_detail)
-router.get('./all_data', users.all_data)
-router.get('./data/:id', users.single_data)
+router.get('/all_data', users.all_data)
+router.get('/data/:id', users.single_data)
 module.exports={
    router
 }
